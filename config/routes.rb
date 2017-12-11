5b8d639258173985c21c20e2be7c54992f375a6c
Etienne Delorieux <etienne.delorieux@gmail.com>
2017-12-11 18:51:04 +0000

    Creation of the My Trips page

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,7 @@ Rails.application.routes.draw do
   
   get '/preload' => 'reservations#preload'
   get '/preview' => 'reservations#preview'
+  get 'your_trips' => 'reservations#your_trips'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
